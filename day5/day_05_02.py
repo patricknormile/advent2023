526663229c32b7f9898490f445cd857b555140f3
patricknormile <pnormile@parsyl.com>
2023-12-08 12:24:21 -0700

    finally got 5.2 with rough solution, not optimal

--- day5/day_05_02.py
+++ day5/day_05_02.py
@@ -16,7 +16,7 @@ def parse_seeds(seeds) :
     n = len(seedssteps)
     for x in range(0, n, 2) : 
         for i in range(seedssteps[x+1]) : 
-            if i % 50 == 0 : 
+            if i % 5000 == 0 : 
                 yield seedssteps[x] + i
 
 def get_shortest(seeds_str, *args) : 
@@ -30,14 +30,31 @@ def get_shortest(seeds_str, *args) :
             path.append(adds)
         if path[-1] < shortest : 
             shortest = path[-1]
+            seed = s
             print(f"at seed {s}, shortest value {shortest}")
-    return shortest
+    return seed, shortest
+
+def down_from_shortest_seed(seed, shortest, *args) : 
+    new_shortest = shortest 
+    for i in range(5000) : 
+        s = seed - i
+        path = [s]
+        for a in args : 
+            last = path[-1]
+            adds = dict_lookup(a, last)
+            path.append(adds)
+        if path[-1] < new_shortest : 
+            new_shortest = path[-1]
+            print(f"at seed {s}, shortest value {new_shortest}")
+    return new_shortest
 
 def main() : 
     data = load_data("day5/day_5_data.txt")
     splits = split_data(data)
     seeds, maps = splits[0], splits[1:]
-    print(get_shortest(seeds, *maps))
+    seed, short = get_shortest(seeds, *maps)
+    shortest = down_from_shortest_seed(seed, short, *maps)
+    print(shortest)
     
 
 if __name__ == "__main__" : 
